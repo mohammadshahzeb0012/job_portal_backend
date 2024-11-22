35a0be3e29ab93e92af91a273b2919244acd94e6
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-11-23 00:42:48 +0530

    cors fixed

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ const userModel = require("./models/user.model");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("home")
