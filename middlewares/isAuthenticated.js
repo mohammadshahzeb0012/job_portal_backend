dd9ea6cd14cae021b2409503d21c81f27fdffdfd
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-12-20 15:17:42 +0530

    aded jwt

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,46 +1,29 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req?.cookies?.token || req?.headers["authorization"]?.split(" ")[1]
+        const token = req?.cookies?.token || req?.headers["authorization"]?.slice(8);
         if (!token) {
             return res.status(401).json({
-                message: "Session expired, please log in again",
+                message: "Session expired please login again",
                 success: false,
-            });
+            })
         }
-
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
-        if (!decoded) {
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
             return res.status(401).json({
-                message: "Session expired, please log in again",
-                success: false,
-            });
-        }
-
-        req.userId = decoded.userId;
-
+                message:"Session expired please login again",
+                success:false
+            })
+        };
+        req.id = decode.userId;
         next();
     } catch (err) {
-        if (err.name === 'TokenExpiredError') {
-            return res.status(401).json({
-                message: "Session expired, please log in again",
-                success: false,
-            });
-        }
-        if (err.name === 'JsonWebTokenError') {
-            return res.status(400).json({
-                message: "Invalid token, please log in again",
-                success: false,
-            });
-        }
-        
-        return res.status(500).json({
-            message: "An unexpected error occurred, please try again later",
-            success: false,
-        });
+        return res.status(401).json({
+            message:"Session expired please login again",
+            success:false
+        })
     }
-};
+}
 
-module.exports = isAuthenticated;
+module.exports = isAuthenticated
