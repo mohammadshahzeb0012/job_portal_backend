c06723f5ef2b2dbd310d029b647f59226040c99e
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-12-20 15:04:55 +0530

    fixed aut middleware

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,34 +3,49 @@ const jwt = require("jsonwebtoken")
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req?.cookies?.token || req?.headers["authorization"]?.slice(8);
+        // if (!token) {
+        //     return res.status(401).json({
+        //         message: "User not authenticated",
+        //         success: false,
+        //     })
+        // }
+        // const decode =  jwt.verify(token, process.env.SECRET_KEY)
+        // if (!decode) {
+        //     return res.status(401).json({
+        //         message: "Invalid token",
+        //         success: false
+        //     })
+        // };
+        // req.id = decode.userId;
+
         if (!token) {
             return res.status(401).json({
-                message: "User not authenticated",
+                message: "User not authenticated pleae login again",
                 success: false,
             })
         }
-        const decode =  jwt.verify(token, process.env.SECRET_KEY)
-        if (!decode) {
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
             return res.status(401).json({
-                message: "Invalid token",
-                success: false
+                message:"Invalid token please login again",
+                success:false
             })
         };
         req.id = decode.userId;
         next();
     } catch (err) {
-        if (err instanceof jwt.TokenExpiredError) {
-            return res.status(401).json({
-                message: "token expired",
-                success: false
-            })
-        }
-        if (err instanceof jwt.JsonWebTokenError) {
-            return res.status(401).json({
-                message: "Invalid token",
-                success: false
-            })
-        }
+        // if (err instanceof jwt.TokenExpiredError) {
+        //     return res.status(401).json({
+        //         message: "token expired",
+        //         success: false
+        //     })
+        // }
+        // if (err instanceof jwt.JsonWebTokenError) {
+        //     return res.status(401).json({
+        //         message: "Invalid token",
+        //         success: false
+        //     })
+        // }
     }
 }
 
